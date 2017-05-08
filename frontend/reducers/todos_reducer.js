65b9fc059eb485c9d685e6df2bff47c50a03ad7e
Trevor OConnor <trevoroc22@gmail.com>
2017-05-08 11:27:18 -0700

    Create store in entry and set up some initial todos for testing

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,4 +1,19 @@
-export function todosReducer(state = {}, action) {
+const initialState = {
+  1: {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false
+  },
+  2: {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true
+  }
+};
+
+export function todosReducer(state = initialState, action) {
   switch(action.type) {
     default:
       return state;
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store.js';
+
+const store = configureStore();
+window.store = store;
 
 document.addEventListener("DOMContentLoaded", () => {
   let root = document.getElementById("root");
