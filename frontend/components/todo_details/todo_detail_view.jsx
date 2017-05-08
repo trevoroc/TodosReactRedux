dec8ba66602f66d806b7c0b4eda4f6484c46c87d
Trevor OConnor <trevoroc22@gmail.com>
2017-05-08 16:44:44 -0700

    Add checkboxes to change status, refactor todo details into separate component

---
+++ frontend/components/todo_details/todo_detail_view.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class TodoDetailView extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleDelete(e) {
+    e.preventDefault();
+    this.props.removeTodo(this.props.todo);
+  }
+
+  render() {
+    return (
+      <article>
+        <p>{ this.props.todo.body }</p>
+        <button onClick={this.handleDelete}>Delete</button>
+      </article>
+    );
+  }
+}
+
+export default TodoDetailView;
--- frontend/components/todo_list/todo_list_item.jsx
+++ frontend/components/todo_list/todo_list_item.jsx
@@ -1,30 +1,40 @@
 import React from 'react';
+import TodoDetailViewContainer
+  from '../todo_details/todo_detail_view_container';
 
 class TodoListItem extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = { detail: false };
+
     this.handleToggle = this.handleToggle.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
+    this.handleDetail = this.handleDetail.bind(this);
   }
 
   handleToggle(e) {
-    e.preventDefault();
     this.props.toggleTodo(this.props.todo);
   }
 
-  handleDelete(e) {
-    e.preventDefault();
-    this.props.removeTodo(this.props.todo);
+  handleDetail(e) {
+    this.setState({ detail: !this.state.detail });
   }
 
   render() {
-    let toggleText = this.props.todo.done ? "Mark Undone" : "Mark Done";
+    let toggleText = this.props.todo.done
+                      ? "Mark Undone" : "Mark Done";
+    let details = this.state.detail
+            ? <TodoDetailViewContainer todo={ this.props.todo }/>
+            : "";
     return (
       <li>
-        {this.props.todo.title}
-        <button onClick={this.handleToggle}>{ toggleText }</button>
-        <button onClick={this.handleDelete}>Delete</button>
+        <input onChange={this.handleToggle}
+               type="checkbox"
+               checked={this.props.todo.done}></input>
+        <span onClick={this.handleDetail}>
+          {this.props.todo.title}
+        </span>
+        { details }
       </li>
     );
   }
