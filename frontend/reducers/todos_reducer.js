10f384d60c6d103fb0a9a90771a381fa641171a5
Trevor OConnor <trevoroc22@gmail.com>
2017-05-08 11:56:24 -0700

    Add actions to todosReducer

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,3 +1,5 @@
+import { RECEIVE_TODOS, RECEIVE_TODO } from '../actions/todo_actions.js';
+
 const initialState = {
   1: {
     id: 1,
@@ -15,6 +17,12 @@ const initialState = {
 
 export function todosReducer(state = initialState, action) {
   switch(action.type) {
+    case RECEIVE_TODOS:
+      return Object.assign({}, state, action.todos);
+    case RECEIVE_TODO:
+      const newState = {};
+      newState[action.todo.id] = action.todo;
+      return Object.assign({}, state, newState);
     default:
       return state;
   }
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store.js';
+import { receiveTodos, receiveTodo } from './actions/todo_actions';
 
 const store = configureStore();
+window.store = store;
+window.receiveTodos = receiveTodos;
+window.receiveTodo = receiveTodo;
 
 document.addEventListener("DOMContentLoaded", () => {
   let root = document.getElementById("root");
